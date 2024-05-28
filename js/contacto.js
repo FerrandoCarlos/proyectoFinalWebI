de7c6dcf918a3397fbb2690181aff4fe9ceed7f9
FerrandoCarlos <cferrando2015@gmail.com>
2024-05-28 09:08:09 -0300

    Cambios

--- js/contacto.js
+++ js/contacto.js
@@ -3,121 +3,92 @@ const regExNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
 const regExEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 const regExTel = /^\d{7,14}$/;
 //se recuperan los campos 
-
 let nombreInput = document.getElementById('nombre');
 let emailInput = document.getElementById('email');
 let telInput = document.getElementById('tel');
-let emailOblig = document.getElementById('email-oblig');
-let nombreOblig = document.getElementById('nombre-oblig');
+
 
 let validarNombre = document.getElementById('validarNombre');
 let validarEmail = document.getElementById('validarEmail');
 let validarTel = document.getElementById('validarTel');
+let txtNombre = document.getElementById('txtNombre');
+let txtEmail = document.getElementById('txtEmail');
+let txtTel = document.getElementById('txtTel');
 const datos = [];//Array para recuperar datos
 
 
 
 
 function validar() {
+
     valNombre();
     valEmail();
     valTel();
-
     return false;
 }
 
-function valNombre() {
-    let icono = document.createElement("i");
-    let parrafo = document.createElement("small");
-    let nombreTxt = nombreInput.value.trim();
+function validarCampo(input, regex, valida, container, obligElemento, exitoMsg, errorMsg) {
 
-    if (nombreTxt == '') {
+    const parrafo = document.createElement("small");
+    const valor = input.value.trim();
 
-        icono.classList.add("validar", "fa", "fa-times-circle", "error");
-        validarNombre.appendChild(icono);
-        parrafo.innerHTML = "El campo no puede estar vacío";
-        parrafo.classList.add("parrafo-error");
-        validarNombre.appendChild(parrafo);
-        nombreOblig.style.visibility = "visible";
-
-    } else if (!regExNombre.test(nombreTxt)) {
-
-        icono.classList.add("validar", "fa", "fa-times-circle", "error");
-        validarNombre.appendChild(icono);
-        parrafo.innerHTML = "Ingrese un nombre valido";
-        parrafo.classList.add("parrafo-error");
-        validarNombre.appendChild(parrafo);
-
-    } else if (regExNombre.test(nombreTxt)) {
-
+    if (valor === '') {                  //se revisa valores vacíos
+        if (obligElemento !== null) {
+            icono.classList.add("validar", "fa", "fa-times-circle", "error");
+            parrafo.textContent = "El campo no puede ser vacío";
+            obligElemento.style.visibility = "visible";
+        }
+    } else if (regex.test(valor)) {
+        icono.classList.remove("validar", "fa", "fa-times-circle", "error");     //se valida por si el campo no cumple con la expresión regular
         icono.classList.add("validar", "fa", "fa-check-circle", "exito");
-        validarNombre.appendChild(icono);
-        parrafo.innerHTML = "El nombre ingresado es correcto";
-        parrafo.classList.add("parrafo-exito");
-        validarNombre.appendChild(parrafo);
-        datos.push(nombreTxt);
-
+        parrafo.textContent = exitoMsg;
+        datos.push(valor);                //Se guardan los datos
+    } else {
+        icono.classList.remove("validar", "fa", "fa-check-circle", "exito");
+        icono.classList.remove("validar", "fa", "fa-times-circle", "error");     //si no valida casos anteriores es campo es correcto 
+        parrafo.textContent = errorMsg;
     }
+
+    parrafo.classList.add(valor === '' || !regex.test(valor) ? "parrafo-error" : "parrafo-exito");  //se agrega clase de error o exito al small 
+
+    container.appendChild(parrafo);
+
+}
+
+function valNombre() {
+    validarCampo(
+        nombreInput,
+        regExNombre,
+        validarNombre,
+        txtNombre,
+        "si",
+        "El nombre ingresado es correcto",
+        "Ingrese un nombre válido");
 
 }
 
 function valEmail() {
-    let icono = document.createElement("i");
-    let parrafo = document.createElement("small");
-    let emailTxt = emailInput.value.trim();
-
-
-    if (emailTxt == '') {
-
-        icono.classList.add("validar", "fa", "fa-times-circle", "error");
-        validarEmail.appendChild(icono);
-        parrafo.innerHTML = "El campo no puede estar vacío";
-        parrafo.classList.add("parrafo-error");
-        validarEmail.appendChild(parrafo);
-        emailOblig.style.visibility = "visible";
-
-    } else if (!regExEmail.test(emailTxt)) {
-
-        icono.classList.add("validar", "fa", "fa-times-circle", "error");
-        validarEmail.appendChild(icono);
-        parrafo.innerHTML = "Ingrese un email valido";
-        parrafo.classList.add("parrafo-error");
-        validarEmail.appendChild(parrafo);
-
-    } else if (regExEmail.test(emailTxt)) {
-
-        icono.classList.add("validar", "fa", "fa-check-circle", "exito");
-        validarEmail.appendChild(icono);
-        parrafo.innerHTML = "El email ingresado es correcto";
-        parrafo.classList.add("parrafo-exito");
-        validarEmail.appendChild(parrafo);
-        datos.push(emailTxt);
-    }
+    validarCampo(
+        emailInput,
+        regExEmail,
+        validarEmail,
+        txtEmail,
+        "si",
+        "El email ingresado es correcto",
+        "Ingrese un email válido"
+    );
 }
 
 function valTel() {
-    let icono = document.createElement("i");
-    let parrafo = document.createElement("small");
-    let telTxt = Number(telInput.value.trim());
-    console.log(telTxt);
-    if (!regExTel.test(telTxt)) {
-
-        icono.classList.add("validar", "fa", "fa-times-circle", "error");
-        validarTel.appendChild(icono);
-        parrafo.innerHTML = "Ingrese un número valido";
-        parrafo.classList.add("parrafo-error");
-        validarTel.appendChild(parrafo);
-
-    } else if (regExTel.test(telTxt)) {
-
-        icono.classList.add("validar", "fa", "fa-check-circle", "exito");
-        validarTel.appendChild(icono);
-        parrafo.innerHTML = "El número ingresado es correcto";
-        parrafo.classList.add("parrafo-exito");
-        validarTel.appendChild(parrafo);
-        datos.push(telTxt);
-
-    }
+    validarCampo(
+        telInput,
+        regExTel,
+        validarTel,
+        txtTel,
+        null,
+        "El teléfono ingresado es correcto",
+        "Ingrese un teléfono válido"
+    );
 }
 
 
